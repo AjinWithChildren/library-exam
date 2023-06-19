07985a0116d0fdeedc7709862945ad6c04d25b7f
EomAJin <dkwls2326@naver.com>
2023-06-19 11:40:33 +0900

    [feat] : branch test commit

--- src/main/java/bit/edu/exam/Main.java
+++ src/main/java/bit/edu/exam/Main.java
@@ -9,5 +9,7 @@ package bit.edu.exam;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("branch test");
     }
 }
