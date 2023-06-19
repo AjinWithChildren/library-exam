5979e0b3520e07f1f34c568afc464bf6f7a4d4d9
gwanii <lung4536@gmail.com>
2023-06-19 10:18:24 +0900

    first commit

---
+++ src/main/java/bit/edu/exam/Main.java
@@ -0,0 +1,13 @@
+package bit.edu.exam;
+
+/**
+ * Some description here.
+ *
+ * @author : 강명관
+ * @since : 1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
