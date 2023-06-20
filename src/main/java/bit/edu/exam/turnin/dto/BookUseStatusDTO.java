c12ad312bb6811fb17395f88002bc1185ef2b69f
GongJinYoung <wlsdud3297@naver.com>
2023-06-20 20:48:17 +0900

    FEAT : 반납 부분 생성,조회 부분 완료

---
+++ src/main/java/bit/edu/exam/turnin/dto/BookUseStatusDTO.java
@@ -0,0 +1,12 @@
+package bit.edu.exam.turnin.dto;
+
+import java.util.Date;
+
+public class BookUseStatusDTO {
+
+    private String bookSeq;
+    private String userId;
+    private Date borrowStart;
+    private Date borrowEnd;
+    private Date returnDate;
+}
